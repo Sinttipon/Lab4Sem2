676b2df656c724ecf80fb9469486d9a009663108
Sinttipon <vdanvik@mail.ru>
2026-08-04 17:53:17 +0300

    class Option

---
+++ include/core/option.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <exceptions.hpp>
+
+template <typename T>
+class Option
+{
+private:
+    bool hasValue;
+    T value;
+public:
+    Option() : hasValue(false) {}
+    Option(const T &val) : hasValue(true), value(val) {}
+
+    bool HasValue() const {return hasValue;}
+
+    T GetValue() const
+    {
+        if (!hasValue)
+            throw InvalidArgument("Option не содержит значения");
+        return value;
+    }
+
+    T GetValueOrDefault(const T &def) const
+    {
+        return hasValue ? value : def;
+    }
+};
