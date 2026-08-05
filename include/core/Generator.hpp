4a485148682df98f305758ae65c35631064b3915
Sinttipon <vdanvik@mail.ru>
2026-08-05 23:44:13 +0300

    generator

---
+++ include/core/Generator.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include "Cardinal.hpp"
+#include "Option.hpp"
+#include <memory>
+
+template <typename T>
+class Generator
+{
+public:
+    virtual ~Generator() = default;
+
+    virtual T GetNext() = 0;
+    virtual bool HasNext() const = 0;
+    virtual Cardinal GetPotentialSize() const = 0;
+
+    virtual Option<T> TryGetNext()
+    {
+        if (HasNext())
+            return Option<T>(GetNext());
+        return Option<T>();
+    }
+
+    virtual Generator<T> *Clone() const = 0;
+};
